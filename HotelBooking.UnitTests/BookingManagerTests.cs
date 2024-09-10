88fdfe00d4e9bb102e18909ccadcec390079e9e7
Rasmus Sandbæk <sandbxk@gmail.com>
2024-09-10 10:54:24 +0200

    ,

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -19,6 +19,8 @@ namespace HotelBooking.UnitTests
             IRepository<Room> roomRepository = new FakeRoomRepository();
             bookingManager = new BookingManager(bookingRepository, roomRepository);
         }
+        
+        
 
         [Fact]
         public void FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException()
---
+++ HotelBooking.UnitTests/Fakes/RepositoryMockFactory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using HotelBooking.Core;
+using Moq;
+
+namespace HotelBooking.UnitTests.Fakes;
+
+public static class RepositoryMockFactory
+{
+    public static Mock<IRepository<T>> CreateMockRepository<T>(IEnumerable<T> initialData = null)
+    {
+        var mockRepo = new Mock<IRepository<T>>();
+
+        // Local data store to simulate repository state
+        var dataStore = initialData != null ? new List<T>(initialData) : new List<T>();
+
+        // Setup GetAll() method
+        mockRepo.Setup(repo => repo.GetAll()).Returns(() => dataStore);
+
+        // Setup Get(id) method
+        mockRepo.Setup(repo => repo.Get(It.IsAny<int>()))
+            .Returns<int>(id => dataStore.Count > id ? dataStore[id] : default);
+
+        // Setup Add(entity) method
+        mockRepo.Setup(repo => repo.Add(It.IsAny<T>()))
+            .Callback<T>(entity => dataStore.Add(entity));
+
+        // Setup Edit(entity) method
+        mockRepo.Setup(repo => repo.Edit(It.IsAny<T>()))
+            .Callback<T>(entity =>
+            {
+                var index = dataStore.IndexOf(entity);
+                if (index != -1)
+                {
+                    dataStore[index] = entity;
+                }
+            });
+
+        // Setup Remove(id) method
+        mockRepo.Setup(repo => repo.Remove(It.IsAny<int>()))
+            .Callback<int>(id =>
+            {
+                if (id >= 0 && id < dataStore.Count)
+                {
+                    dataStore.RemoveAt(id);
+                }
+            });
+
+        return mockRepo;
+    }
+}
