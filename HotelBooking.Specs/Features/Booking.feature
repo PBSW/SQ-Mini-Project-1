46ac53097053e686effb8c311b756ed5e09e1874
Philip Zadeh <philezad@gmail.com>
2024-11-04 12:34:43 +0100

    Finished first scenario

--- HotelBooking.Specs/Features/Booking.feature
+++ HotelBooking.Specs/Features/Booking.feature
@@ -1,7 +1,7 @@
 Feature: Create Booking
 
     Scenario: Book a room with a valid future date range
-        Given there are available rooms on given date
+        Given there are available rooms 3 days from now till 5 days from now
         When the user attempts to book a room
         Then the booking is created successfully
        
--- HotelBooking.Specs/StepDefinitions/BookingStepDefinitions.cs
+++ HotelBooking.Specs/StepDefinitions/BookingStepDefinitions.cs
@@ -21,15 +21,16 @@ public sealed class BookingStepDefinitions
         _roomRepo = new Mock<IRepository<Room>>();
         
         _bookingManager = new BookingManager(_bookRepo.Object, _roomRepo.Object);
+        
+        RoomSetup();
     }
     
     // For additional details on Reqnroll step definitions see https://go.reqnroll.net/doc-stepdef
 
-    [Given("there are available rooms on given date")]
-    public void GivenThereAreAvailableRoomsOnDate()
+    [Given("there are available rooms {int} days from now till {int} days from now")]
+    public void GivenThereAreAvailableRoomsOnDate(int daysFromNow, int daysToNow)
     {
-        RoomSetup();
-        _bookingManager.FindAvailableRoom(DateTime.Now.AddDays(2), DateTime.Now.AddDays(5));
+        _bookingManager.FindAvailableRoom(DateTime.Now.AddDays(daysFromNow), DateTime.Now.AddDays(daysToNow));
     }
 
     [When("the user attempts to book a room")]
@@ -61,4 +62,23 @@ public sealed class BookingStepDefinitions
         
         _roomRepo.Setup(repo => repo.GetAll()).Returns(rooms);
     }
+
+    private void BookingsSetup()
+    {
+        var bookings = new List<Booking>
+        {
+            new Booking
+            {
+                Id = 1, StartDate = DateTime.Today.AddDays(1), EndDate = DateTime.Today.AddDays(3), IsActive = true,
+                CustomerId = 1, RoomId = 1
+            },
+            new Booking
+            {
+                Id = 2, StartDate = DateTime.Today.AddDays(3), EndDate = DateTime.Today.AddDays(6), IsActive = true,
+                CustomerId = 2, RoomId = 2
+            }
+        };
+        
+            _bookRepo.Setup(repo => repo.GetAll()).Returns(bookings);
+        }
 }
